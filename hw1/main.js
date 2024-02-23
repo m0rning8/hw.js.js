2dadfea70e7c6d793862cb09b8edb0b2243b3ee6
m0rning8 <terryoneloveyou@gmail.com>
2024-02-23 03:10:38 +0100

    зробив дз 2.0

---
+++ hw1/main.js
@@ -0,0 +1,48 @@
+//'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+let a = "hello"
+console.log(a)
+let b = "owu"
+console.log(b)
+let c = "com"
+console.log(c)
+let d = "ua"
+console.log(d)
+let e = 1
+console.log(e)
+let f = 10
+console.log(f)
+let g = -999
+console.log(g)
+let h = 123
+console.log(h)
+let i = 3.14
+console.log(i)
+let j = 2.7
+console.log(j)
+let k = 16
+console.log(k)
+let l = true
+console.log(l)
+let m = false
+console.log(m)
+
+let firstname = "Tymofii"
+let middlename = "Oleksiyovich"
+let lastname = "Kazantsev"
+let person = `${lastname} ${firstname} ${middlename}`
+console.log(person)
+
+let a1 = 100;
+let b1 = '100';
+let c1 = true;
+console.log(typeof a1)
+console.log(typeof b1)
+console.log(typeof c1)
+
+let name = prompt("Enter your name")
+console.log(name)
+let midname = prompt("Enter your middle name")
+console.log(midname)
+let age = prompt("Enter your age")
+console.log(age)
+
