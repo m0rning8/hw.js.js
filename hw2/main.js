6cd13a43147d9c77ed9e0e1d2b53b3fc42534921
m0rning8 <terryoneloveyou@gmail.com>
2024-02-27 03:38:46 +0100

    зробив дз 2.0

--- hw2/main.js
+++ hw2/main.js
@@ -123,4 +123,34 @@ switch (timetable.daynumber){
         console.log(timetable.dayname7);
         break;
 }
-//
+//- є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
+// за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
+let x1 = 0 || null || undefined || NaN || "" || "default"
+console.log(x1)
+//- з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+if (coursesAndDurationArray[0].monthDuration > 5){
+    console.log('Супер1')
+}
+if (coursesAndDurationArray[1].monthDuration > 5){
+    console.log('Супер2')
+}
+if (coursesAndDurationArray[2].monthDuration > 5){
+    console.log('Супер3')
+}
+if (coursesAndDurationArray[3].monthDuration > 5){
+    console.log('Супер4')
+}
+if (coursesAndDurationArray[4].monthDuration > 5){
+    console.log('Супер5')
+}
+if (coursesAndDurationArray[5].monthDuration > 5){
+    console.log('Супер6')
+}
